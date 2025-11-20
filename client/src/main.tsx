b34419135a4e6e0f7aad676078b4500e73db30e3
keigo810 <keigo428kyudo@gmail.com>
2025-11-20 17:47:53 +0900

    Fix build: close FinanceLearn main and load analytics script conditionally

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const analyticsEndpoint = import.meta.env.VITE_ANALYTICS_ENDPOINT;
+const analyticsWebsiteId = import.meta.env.VITE_ANALYTICS_WEBSITE_ID;
+
+if (analyticsEndpoint && analyticsWebsiteId && typeof document !== "undefined") {
+  const script = document.createElement("script");
+  script.src = `${analyticsEndpoint.replace(/\/$/, "")}/umami`;
+  script.async = true;
+  script.defer = true;
+  script.dataset.websiteId = analyticsWebsiteId;
+  document.body.appendChild(script);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
